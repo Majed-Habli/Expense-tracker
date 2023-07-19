99ed0222fc95c82584ec5179c4e7afb0324f3100
majed <majedhabli2020@gmail.com>
2023-07-19 16:34:48 +0300

    Linked the javascript to the html and wrote addItem function.

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,35 @@ function todoItemElement(text, amount) {
   return `<li>
     <span class="text">${text}</span>
     <span class="text">${amount}</span>
-    <span class="remove">&#128465</span>
+    <span class="remove">&#128465 delete</span>
   </li>`
 }
+
+function addItem() {
+  const ItemInput = $("#expense-name-input");
+  const AmountInput = $("#expenses-input");
+  const expenseList = $("#expenses-list");
+
+  if (ItemInput.val().trim() === "") return;
+
+  const todoItem = $(todoItemElement(ItemInput.val(), AmountInput.val()))
+
+  todoItem.find(".remove").click(function () {
+    todoItem.remove()
+  })
+
+  expenseList.append(todoItem)
+  ItemInput.val("")
+}
+
+$(document).ready(function () {
+  const addButton = $("#add-button");
+  addButton.click(addItem);
+
+  $("#expense-name-input").keyup(function (event) {
+    if (event.keyCode === 13) {
+      addItem()
+    }
+  }
+  )
+})
